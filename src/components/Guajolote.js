eb360bf769eaebd4ee008ec32132b9f97ddd354e
andres cagua <andres.fcr089@gmail.com>
2022-01-25 23:00:04 -0500

    hello

--- src/components/Guajolote.js
+++ src/components/Guajolote.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
 import {  urlGuajolota } from '../helpers/Url';
 import { Foto } from '../styles/GuajolotesStyles';
-
+import Counter from './Contador';
+import Home from './Home'
 
 
 export default class Guajolote extends Component {
@@ -32,8 +33,10 @@ export default class Guajolote extends Component {
         const state = this.state.productos
 
         return <div>
+            <Home/>
             {
                 state.map(product => (
+                
                     <Card key={product.id}>
                     <Foto variant="top" src={product.imagen} />
                     <Card.Body>
@@ -42,11 +45,10 @@ export default class Guajolote extends Component {
                             {product.precio}
                         </Card.Text>
                     </Card.Body>
+                    <Counter key={product.id}/>
                 </Card>
-                ) )
-                
+                )) 
             };
-            <Contador/>
         </div>
     }
 
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Formulario from '../components/Formulario';
 
 import App from '../containers/App';
 
@@ -11,7 +12,7 @@ export default class AppRouter extends Component {
         <Routes>
             <Route path="/" element={<App/>}/>
             <Route path="*" element={<Navigate to ="/"/>}/>
-            
+            <Route path="/crearCuenta" element={<Formulario/>}/>
         </Routes>
         </BrowserRouter>
     ) 
