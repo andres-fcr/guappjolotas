2bf0c863b2fc76834cef1d8fb4f22f33233642a5
Alexandra Sivira <kerensivira@gmail.com>
2022-01-25 23:46:26 -0500

    j

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (<div>
-        <h1>Bienvenid@ a</h1>
+        <h1>Bienvenido.</h1>
         <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
         <Button type='button'>Login</Button>
         <Link to="/crearCuenta"><Button type='button'>Crear cuenta</Button></Link>
