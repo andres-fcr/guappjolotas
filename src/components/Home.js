63616f8d7192f31c02857338866b85f13666a4dc
Alexandra Sivira <kerensivira@gmail.com>
2022-01-25 23:42:19 -0500

    h

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,6 @@ const Home = () => {
         <h1>Bienvenid@ a</h1>
         <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
         <Button type='button'>Login</Button>
-<<<<<<< HEAD
-        
-=======
->>>>>>> 36e058bafe73b3ba4817c545e7875f4e8c97670a
         <Link to="/crearCuenta"><Button type='button'>Crear cuenta</Button></Link>
     </div>);
 };
