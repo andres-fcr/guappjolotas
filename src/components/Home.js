ed32d84b5c2f285ed9596755bb7ec3d58b174fb9
Alexandra Sivira <kerensivira@gmail.com>
2022-01-27 16:22:44 -0500

    23

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Bienvenida } from '../styles/HomeStyles';
 
 const Home = () => {
     return (<div>
-        <h1>Bienvenido.</h1>
+        <Bienvenida>Bienvenido.</Bienvenida>
         <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
         <Button type='button'>Login</Button>
         <Link to="/crearCuenta"><Button type='button'>Crear cuenta</Button></Link>
