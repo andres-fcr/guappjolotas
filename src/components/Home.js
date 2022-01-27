a1763dcd326da2306eeeaba494caf2bd781f013c
Alexandra Sivira <kerensivira@gmail.com>
2022-01-27 18:19:45 -0500

    y

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Bienvenida } from '../styles/HomeStyles';
+import { Bienvenida, Centrar } from '../styles/HomeStyles';
 
 const Home = () => {
     return (<div>
-        <Bienvenida>Bienvenido.</Bienvenida>
-        <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
+        
+        <Centrar>
+        <h1>Bienvenido.</h1>
+        <Bienvenida src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
         <Button type='button'>Login</Button>
         <Link to="/crearCuenta"><Button type='button'>Crear cuenta</Button></Link>
+        </Centrar>
+        
     </div>);
 };
 
