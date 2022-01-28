4e2cfa36cc60127e7a9dfff163ada9032df7d6d2
Carlos <carlosrenmat@gmail.com>
2022-01-27 20:09:27 -0500

    listo

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <Centrar>
         <h1>Bienvenido.</h1>
         <Bienvenida src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' />
-        <Button type='button'>Login</Button>
+       <Link to="/iniciarSecion"><Button type='button'>Iniciar secion</Button></Link> <br></br>
         <Link to="/crearCuenta"><Button type='button'>Crear cuenta</Button></Link>
         </Centrar>
         
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Formulario from '../components/Formulario';
 import Home from '../components/Home';
+import Login from '../components/Login';
 
 export default class AppRouter extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class AppRouter extends Component {
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/crearCuenta" element={<Formulario />} />
+          <Route path="/iniciarSecion" element={<Login />} />
         </Routes>
       </BrowserRouter>
     )
