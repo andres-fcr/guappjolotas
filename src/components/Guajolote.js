569147776ffb8ac6f937a7be24aabeb6bffea032
andres cagua <andres.fcr089@gmail.com>
2022-01-27 20:13:41 -0500

    asd

--- src/components/Guajolote.js
+++ src/components/Guajolote.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { urlGuajolota } from '../helpers/Url';
 import { Foto, Li, ListProducto, Precio, Producto, TProducto, Ul, Head } from '../styles/GuajolotesStyles';
 
@@ -26,20 +27,20 @@ const Guajolote = () => {
             })
     }
 
-    const listar = productos.map(function (element) {
-        return element.id > 2;
-    })
-    console.log(listar);
+    const listar = productos.map(prod => prod.clase === lista)
+    
 
     return (
         <div>
             <Head>
-                <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' width={62} height="auto" />
+                <Link to="/">
+                    <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png" alt='' width={62} height="auto" />
+                </Link>
                 <img src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700043/guappjolotas/carrito_mlxzjd.png" alt="" width={20} height="auto" />
                 <h1>Nada como una Guajolota para empezar el dia</h1>
                 <input type="text" />
             </Head>
-            <Ul class="row">
+            <Ul className='row'>
                 <Li><a href="/">Guajolotas</a></Li>
                 <Li><a href="/">Bebidas</a></Li>
                 <Li><a href="/">Tamales</a></Li>
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Formulario from '../components/Formulario';
+import Guajolote from '../components/Guajolote';
 import Home from '../components/Home';
+import Login from '../components/Login';
 
 export default class AppRouter extends Component {
   render() {
@@ -11,6 +13,8 @@ export default class AppRouter extends Component {
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/crearCuenta" element={<Formulario />} />
+          <Route path="/productos" element={<Guajolote />}/>
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     )
