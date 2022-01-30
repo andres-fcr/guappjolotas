c40adc799ecc6212732b0890f6a6da3e45bc3cbd
Carlos <carlosrenmat@gmail.com>
2022-01-30 11:09:41 -0500

    hola

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Collapse } from 'react-bootstrap';
+import { Button, Card} from 'react-bootstrap';
 // import { urlGuajolota } from '../helpers/Url';
-import { Foto, Li, ListProducto, Precio, Producto, TProducto, Ul, Head } from '../styles/GuajolotesStyles';
+import { Foto,Li, ListProducto, Precio, Producto, TProducto, Ul, Head } from "../styles/ProductosStyles"
 import accounting from 'accounting';
 // import getData from '../helpers/getData';
 import { Link } from 'react-router-dom';
@@ -11,44 +11,19 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 const Productos = ({ tarea }) => {
-  const [lista, setLista] = useState("Guajalotes");
 
-  const filtered = tarea.filter(function (element) {
-    return element.clase === lista;
-  });
+    const [lista, setLista] = useState("Guajalotes");
 
-  return (
-    <div>
-      <Imagenes>
-        <Link to="/">
-          <img
-            src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700045/guappjolotas/logo_a9tk2c.png"
-            alt=""
-            width={62}
-            height="auto"
-          />
-        </Link>
+    const filtered = tarea.filter(function (element) {
+        return element.clase === lista;
+    });
 
-        <Carrito>
-          <img
-            src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700043/guappjolotas/carrito_mlxzjd.png"
-            alt=""
-            width={20}
-            height="auto"
-          />
-        </Carrito>
-      </Imagenes>
-      <Head>
-        <Texto>
-          <h1>Nada como una Guajolota para empezar el dia</h1>
-        </Texto>
-      </Head>
 
     const [produ, setProdu] = useState([]);
     const [tablaProductos, setTablaProductos] = useState([]);
     const [busqueda, setBusqueda] =useState("")
 
-    const [open, setOpen] = useState(false);
+    
 
     const peticionGet= async()=>{
         await axios.get("https://guappjolota.herokuapp.com/Productos")
@@ -96,29 +71,17 @@ const Productos = ({ tarea }) => {
             </Head>
             <h1>Nada como una Guajolota para empezar el dia</h1>
 
-              <div className=''>
+              <div className='busca'>
                 <input 
                 className='form-control inputBuscar' 
                 value={busqueda} 
                 placeholder='Buscar Productos'
                 onChange={handleChange} 
                 />
-                <button className='btn btn-success'
-                     onClick={() => setOpen(!open)}
-                     aria-controls="example-collapse-text"
-                     aria-expanded={open}
-                >
+                <button className='btn btn-success'>
                     <FontAwesomeIcon icon={faSearch}/>
                 </button>
-                <Collapse in={open} dimension="width">
-                   <div id="example-collapse-text">
-                   <Card body style={{width: '400px'}}>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                labore wes anderson cred nesciunt sapiente ea proident.
-                  </Card>
-                  </div>
-                </Collapse>
+
                 </div>
            
 
@@ -137,36 +100,30 @@ const Productos = ({ tarea }) => {
                 {
                     filtered.map(product => (
 
-      <Ul>
-        <Guajolotes><Button onClick={() => setLista("Guajalotes")}>Guajalotas</Button></Guajolotes> 
-        <Tamales><Button onClick={() => setLista("Tamales")}>Tamales</Button></Tamales> 
-        <Bebidas><Button onClick={() => setLista("Bebidas")}>Bebidas</Button></Bebidas>
-      </Ul>
-      <ListProducto>
-        {filtered.map((product) => (
-          <Producto key={product.id}>
-            <Link to={`/detalle/${product.clase}${product.id}`}>
-              <Card style={{ width: "312px" }} border="light" bg="#FFF">
-                <div className="row">
-                  <div className="col">
-                    <Foto variant="top" src={product.imagen} />
-                  </div>
-                  <div className="col">
-                    <Card.Body style={{ width: "10rem" }}>
-                      <TProducto>{product.name}</TProducto>
-                      <Precio>
-                        {accounting.formatMoney(product.precio, "MXN")}
-                      </Precio>
-                    </Card.Body>
-                  </div>
-                </div>
-              </Card>
-            </Link>
-          </Producto>
-        ))}
-      </ListProducto>
-    </div>
-  );
+                        <Producto key={product.id} >
+                            <Link to={`/detalle/${product.clase}${product.id}`}>
+                                <Card style={{ width: '312px' }} border="light" >
+                                    <div className="row">
+                                        <div className="col">
+                                            <Foto variant="top" src={product.imagen} />
+                                        </div>
+                                        <div className="col">
+                                            <Card.Body style={{ width: '10rem' }}>
+                                                <TProducto>{product.name}</TProducto>
+                                                <Precio>
+                                                    {accounting.formatMoney(product.precio, "MXN")}
+                                                </Precio>
+                                            </Card.Body>
+                                        </div>
+                                    </div>
+                                </Card >
+                            </Link>
+                        </Producto >
+                    ))
+                }
+            </ListProducto>
+        </div >
+    );
 };
 
 export default Productos;
