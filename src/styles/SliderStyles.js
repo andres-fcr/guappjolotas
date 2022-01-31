83d2d7e6fbc355d38da6f60c8f5b2ff14f374f80
Carlos <carlosrenmat@gmail.com>
2022-01-30 21:45:50 -0500

    hi

---
+++ src/styles/SliderStyles.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { Card, } from "react-bootstrap";
+
+
+export const Carro = styled.img`
+float: right;
+margin-top: 30px;
+margin-left: 10px;
+margin-right: 20px;
+width: 40px;
+height: 40px;
+`
+
+export const Foto = styled.img`
+position: static;
+width: 64px;
+height: 69px;
+left: 0px;
+top: 0px;
+flex: none;
+order: 0;
+flex-grow: 0;
+margin: 0px 52px;
+`
+
+export const Cafe = styled(Card)`
+/* margin-top: 50%; */
+`
+
+
+export const Beba = styled(Card.Img)`
+align-items: center;
+border-radius: 20px;
+`
+
+export const Flecha = styled.img`
+position: absolute;
+width: 24px;
+height: 24px;
+left: 24px;
+top: 44px;
+`
+
+export const Coma = styled.img`
+margin-left: 550px;
+margin-top: 50px;
+width: 200px;
+height: 200px;
+left: 80px;
+`
+
+// export const Boto = styled.button`
+// background-color: #FA4A0C;
+
+
+// `
+
+
+
+export const Contenido = styled.div`
+display: flex;
+align-items: flex-start;
+
+`
+
+
+export const Contodo = styled.div`
+display: flex;
+align-items: flex-start;
+flex: none;
+order: 0;
+flex-grow: 0;
+margin: 0px 8px;
+`
