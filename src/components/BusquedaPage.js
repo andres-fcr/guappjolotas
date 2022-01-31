6389c0c09684fda6db488bef1b9abe58edf27fb6
Alexandra Sivira <kerensivira@gmail.com>
2022-01-30 23:34:00 -0500

    mk

--- src/components/BusquedaPage.js
+++ src/components/BusquedaPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Div, Img, Word2 } from '../styles/BusquedaStyles';
 
 const BusquedaPage = () => {
-    return <div>
+    return <Div>
    
-            <h1>Realice su búsqueda.</h1>
-            <img src="https://res.cloudinary.com/alexa01020304/image/upload/v1643515847/pngwing.com_oa3ds0.png" />
+           <Word2><h1>Realice su búsqueda.</h1></Word2>
+            <Img src="https://res.cloudinary.com/alexa01020304/image/upload/v1643515847/pngwing.com_oa3ds0.png" />
        
-    </div>;
+    </Div>;
 };
 
 export default BusquedaPage;
--- src/styles/BusquedaStyles.js
+++ src/styles/BusquedaStyles.js
@@ -1,4 +1,31 @@
 
 
+import styled from "styled-components";
+
+
+export const Div = styled.div`
+
+*{
+    background-color: whitesmoke;
+}
+
+`
+
+export const Word2 = styled.h4`
+
+font-size: 20px;
+text-align: center;
+margin-top: 100px;
+color: grey;
+`
+
+export const Img = styled.img`
+
+position: relative;
+margin-left: 730px;
+margin-top: 100px;
+width: 80px;
+`
+
 
 
