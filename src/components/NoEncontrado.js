bc5b08f64143a7147a2c97e827212b648d223e87
Alexandra Sivira <kerensivira@gmail.com>
2022-01-30 22:22:24 -0500

    nju

---
+++ src/components/NoEncontrado.js
@@ -0,0 +1,9 @@
+
+<Word2>
+    <h1>Producto no encontrado. 
+        Realice su búsqueda nuevamente.</h1>
+</Word2>
+
+<Error>
+<img src = "https://res.cloudinary.com/alexa01020304/image/upload/v1643599308/error_olrwxy.png"/>
+</Error>
